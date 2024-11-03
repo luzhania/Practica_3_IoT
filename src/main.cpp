fed8051732eecf18270dbaac5aa03f5f26028fbb
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-11-03 17:51:21 -0400

    Adding min_pulse_alert and max_pulse_alert

--- src/main.cpp
+++ src/main.cpp
@@ -9,84 +9,168 @@
 #include "WiFiConnection.h"
 
 // Configuración del LCD
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Cambia 0x27 si tu dirección I2C es diferente
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-// Reemplaza con los detalles de tu shadow
-const char* UPDATE_TOPIC = "$aws/things/exercise_band/shadow/update";              // publicar
-const char* UPDATE_DELTA_TOPIC = "$aws/things/exercise_band/shadow/update/delta";  // suscribirse
+// Constantes para MQTT
+const char *UPDATE_TOPIC = "$aws/things/exercise_band/shadow/update";
+const char *UPDATE_DELTA_TOPIC = "$aws/things/exercise_band/shadow/update/delta";
 
+// Variables para los umbrales y mensajes
+unsigned int min_pulse_alert;
+unsigned int max_pulse_alert;
+const char *message;
+const String THING_NAME = "thing1";
+
+// Objetos para WiFi, MQTT y JSON
 WiFiClientSecure wiFiClient;
 PubSubClient client(wiFiClient);
-
-StaticJsonDocument<JSON_OBJECT_SIZE(64)> inputDoc;
-StaticJsonDocument<JSON_OBJECT_SIZE(4)> outputDoc;
+StaticJsonDocument<JSON_OBJECT_SIZE(512)> inputDoc;
+StaticJsonDocument<JSON_OBJECT_SIZE(64)> outputDoc;
 char outputBuffer[128];
 
-// Variables para la medición periódica
+// Variables para el control de tiempo
 unsigned long previousMillis = 0;
 const long interval = 1000; // 1 segundo
 
-void printLCDMessage(const char* message, unsigned int row, unsigned int col) {
+// Función para leer el sensor de pulso
+int readPulseSensor()
+{
+  return random(60, 100); // Simula un valor aleatorio entre 60 y 100
+}
+
+int determinePulseState(int pulse)
+{
+  if (pulse < 60)
+    return 0;
+  if (pulse <= 200)
+    return 1;
+  return 2;
+}
+
+// Función para imprimir mensajes en el LCD
+void printLCDMessage(const char *message, unsigned int row, unsigned int col)
+{
   lcd.clear();
-  lcd.setCursor(col, row); // Posicionar en la primera fila
+  lcd.setCursor(col, row);
   lcd.print(message);
 }
 
-int readPulseSensor() {
-  // int pulse = analogRead(A0); // Asume que el XD-58C está conectado a A0
-  int pulse = random(60, 100); // Simula un valor aleatorio entre 60 y 100
-  return pulse;
-}
-
-string getMessage(unsigned int pulse) {
-  string message;
-  if (pulse < 60) {
-    message = "Pulso demasiado bajo:";
-  } else if (pulse >= 60 && pulse < 100) {
-    message = "Pulso actual:";
-  } else {
-    message = "Pulso demasiado alto:";
-  }
-  return message + to_string(pulse);
-}
-
-void displayPulseOnLCD() {
-  int pulse = readPulseSensor();
-  string message = getMessage(pulse);
-  printLCDMessage(message.c_str(), 0, 0);
-}
-
-void updateShadowPulse(int pulse) {
-  outputDoc["state"]["reported"]["devices"][THING_NAME]["pulse"] = pulse; // Actualiza el pulso en el shadow
+// Función para actualizar el shadow con el valor de pulso
+void updatePulseInShadow(int pulse)
+{
+  outputDoc.clear();
+  outputDoc["state"]["reported"]["devices"][THING_NAME]["heart_rate"] = pulse;
   serializeJson(outputDoc, outputBuffer);
   client.publish(UPDATE_TOPIC, outputBuffer);
 }
 
+// Función para manejar la solicitud de actualización del pulso
+void processPulseRequest(const JsonDocument &doc)
+{
+  int pulse_requested = doc["state"]["devices"][THING_NAME]["pulse_requested"];
+  if (pulse_requested == 1)
+  {
+    int currentPulse = readPulseSensor();
+    updatePulseInShadow(currentPulse);
+  }
+}
+
+void publishMinPulseParameter()
+{
+  outputDoc.clear();
+  outputDoc["state"]["reported"]["devices"][THING_NAME]["min_pulse_alert"] = min_pulse_alert;
+  serializeJson(outputDoc, outputBuffer);
+  client.publish(UPDATE_TOPIC, outputBuffer);
+}
+
+void publishMaxPulseParameter()
+{
+  outputDoc.clear();
+  outputDoc["state"]["reported"]["devices"][THING_NAME]["max_pulse_alert"] = max_pulse_alert;
+  serializeJson(outputDoc, outputBuffer);
+  client.publish(UPDATE_TOPIC, outputBuffer);
+}
+
+void processMinPulseParameter(const JsonDocument &doc)
+{
+  min_pulse_alert = doc["state"]["devices"][THING_NAME]["min_pulse_alert"];
+  publishMinPulseParameter();
+}
+
+void processMaxPulseParameter(const JsonDocument &doc)
+{
+  max_pulse_alert = doc["state"]["devices"][THING_NAME]["max_pulse_alert"];
+  publishMaxPulseParameter();
+}
+
 // Callback para manejar mensajes recibidos
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.println("Llamada a callback");
   String message;
-  for (int i = 0; i < length; i++) message += String((char) payload[i]);
-  Serial.println("Mensaje desde el tema " + String(topic) + ": " + message);
-  
+  for (int i = 0; i < length; i++)
+    message += String((char)payload[i]);
+  Serial.println("Message from topic " + String(topic) + ":" + message);
   DeserializationError err = deserializeJson(inputDoc, payload);
-  if (!err) {
-    if (String(topic) == UPDATE_DELTA_TOPIC) {
-      int pulse_requested = inputDoc["state"]["devices"][THING_NAME]["pulse_requested"].as<int>();
-      if (pulse_requested == 1) {
-        int currentPulse = readPulseSensor();
-        updateShadowPulse(currentPulse); // Actualiza el pulso en el shadow
+
+  if (!err)
+  {
+    if (String(topic) == UPDATE_DELTA_TOPIC)
+    {
+      if (inputDoc["state"]["devices"][THING_NAME]["pulse_requested"])
+      {
+        processPulseRequest(inputDoc);
+      }
+      if (inputDoc["state"]["devices"][THING_NAME]["min_pulse_alert"])
+      {
+        processMinPulseParameter(inputDoc);
+      }
+      if (inputDoc["state"]["devices"][THING_NAME]["max_pulse_alert"])
+      {
+        processMaxPulseParameter(inputDoc);
       }
     }
   }
 }
 
-WiFiConnection wifi(WIFI_SSID, WIFI_PASS);
-const string THING_NAME = "thing1";
+// Función para reconectar con MQTT
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Intentando conexión MQTT...");
+    if (client.connect(CLIENT_ID))
+    {
+      Serial.println("conectado");
+      if (client.subscribe(UPDATE_DELTA_TOPIC))
+        Serial.println("Suscrito a " + String(UPDATE_DELTA_TOPIC));
+    }
+    else
+    {
+      Serial.print("falló, rc=");
+      Serial.print(client.state());
+      Serial.println(" intentando de nuevo en 5 segundos");
+      delay(5000);
+    }
+  }
+}
 
-void setup() {
+// Función para mostrar el pulso en el LCD
+void displayPulseOnLCD()
+{
+  int pulse = readPulseSensor();
+  String message = "Pulso: " + String(pulse);
+  printLCDMessage(message.c_str(), 0, 0);
+}
+
+
+WiFiConnection wifi(WIFI_SSID, WIFI_PASS);
+
+void setup()
+{
   Serial.begin(115200);
-  lcd.init(); // Inicializa el LCD
-  lcd.backlight(); // Enciende la luz de fondo
+  lcd.init();
+  lcd.backlight();
   wifi.connect();
 
   wiFiClient.setCACert(AMAZON_ROOT_CA1);
@@ -97,27 +181,10 @@ void setup() {
   client.setCallback(callback);
 }
 
-void reconnect() {
-  // Bucle hasta que estemos reconectados
-  while (!client.connected()) {
-    Serial.print("Intentando conexión MQTT...");
-    if (client.connect(CLIENT_ID)) {
-      Serial.println("conectado");
-      client.subscribe(UPDATE_DELTA_TOPIC);
-      Serial.println("Suscrito a " + String(UPDATE_DELTA_TOPIC));
-      delay(100);
-    } else {
-      Serial.print("falló, rc=");
-      Serial.print(client.state());
-      Serial.println(" intentando de nuevo en 5 segundos");
-      delay(5000);
-    }
-  }
-}
-
-
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
