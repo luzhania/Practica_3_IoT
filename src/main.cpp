eb4ee8cb7938afb606ceb204d5be5955e24a9fd0
Beto Herbas <betitoherbas@gmail.com>
2024-11-06 22:07:45 -0400

    Esto cuanto tiempo nos va a llevar? !!!! :(

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,22 @@
 
 PulseSensorPlayground pulseSensor;
 
-class LCDDisplay {
+class LCDDisplay
+{
 private:
     LiquidCrystal_I2C lcd;
 
 public:
     LCDDisplay() : lcd(0x27, 20, 4) {}
 
-    void init() {
+    void init()
+    {
         lcd.init();
         lcd.backlight();
     }
 
-    void printMessage(unsigned int pulse, unsigned int row, unsigned int col) {
+    void printMessage(unsigned int pulse, unsigned int row, unsigned int col)
+    {
         lcd.clear();
         lcd.setCursor(col, row);
         lcd.print(pulse);
@@ -31,69 +34,90 @@ public:
 };
 
 // Clase para procesar el estado del pulso
-class MQTTHandler {
+class MQTTHandler
+{
 private:
+    const String THING_NAME = "smartband";
     WiFiClientSecure wiFiClient;
     PubSubClient client;
     StaticJsonDocument<JSON_OBJECT_SIZE(64)> outputDoc;
     char outputBuffer[128];
-    const char *UPDATE_TOPIC = "$aws/things/exercise_band/shadow/update";
-    const char *UPDATE_DELTA_TOPIC = "$aws/things/exercise_band/shadow/update/delta";
-    const String THING_NAME = "thing2";
+
+    String updateTopic = "$aws/things/" + THING_NAME + "/shadow/update";
+    String updateDeltaTopic = "$aws/things/" + THING_NAME + "/shadow/update/delta";
+
+    const char *UPDATE_TOPIC = updateTopic.c_str();
+    const char *UPDATE_DELTA_TOPIC = updateDeltaTopic.c_str();
 
     unsigned int currentState = 0;
     unsigned int minPulseAlert = 60;
     unsigned int maxPulseAlert = 200;
 
-   ///////////
+    ///////////
 
-    void callback(char *topic, byte *payload, unsigned int length) {
+    void callback(char *topic, byte *payload, unsigned int length)
+    {
         String message;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             message += String((char)payload[i]);
         }
         Serial.println("Message from topic " + String(topic) + ": " + message);
-        
+
         StaticJsonDocument<200> inputDoc;
         DeserializationError error = deserializeJson(inputDoc, payload, length);
-        if (!error) {
-            if (String(topic) == UPDATE_DELTA_TOPIC) {
-                if (inputDoc["state"]["devices"][THING_NAME]["pulse_requested"] == 1) {
+        if (!error)
+        {
+            if (String(topic) == UPDATE_DELTA_TOPIC)
+            {
+                if (inputDoc["state"]["pulse_requested"] == 1)
+                {
                     unsigned int pulse = pulseSensor.getBeatsPerMinute();
                     publishPulseRequestAttended();
                     updatePulseInShadow(pulse);
                 }
-                if (inputDoc["state"]["devices"][THING_NAME]["min_pulse_alert"] > 0) {
-                    minPulseAlert = inputDoc["state"]["devices"][THING_NAME]["min_pulse_alert"];
+                if (inputDoc["state"]["min_pulse_alert"] > 0)
+                {
+                    minPulseAlert = inputDoc["state"]["min_pulse_alert"];
                     reportMinPulseParameter();
                 }
-                if (inputDoc["state"]["devices"][THING_NAME]["max_pulse_alert"] > 0) {
-                    maxPulseAlert = inputDoc["state"]["devices"][THING_NAME]["max_pulse_alert"];
+                if (inputDoc["state"]["max_pulse_alert"] > 0)
+                {
+                    maxPulseAlert = inputDoc["state"]["max_pulse_alert"];
                     reportMaxPulseParameter();
                 }
             }
-        } else {
+        }
+        else
+        {
             Serial.println("Error deserializando el mensaje JSON");
         }
     }
 
 public:
-    MQTTHandler(const char *broker, int port) : client(wiFiClient) {
+    MQTTHandler(const char *broker, int port) : client(wiFiClient)
+    {
         client.setServer(broker, port);
-        client.setCallback([this](char *topic, byte *payload, unsigned int length) { this->callback(topic, payload, length); });
+        client.setCallback([this](char *topic, byte *payload, unsigned int length)
+                           { this->callback(topic, payload, length); });
     }
-     
-    unsigned int determinePulseState(unsigned int pulse) {
-        if (pulse < minPulseAlert) return 0;
-        if (pulse < maxPulseAlert) return 1;
+
+    unsigned int determinePulseState(unsigned int pulse)
+    {
+        if (pulse < minPulseAlert)
+            return 0;
+        if (pulse < maxPulseAlert)
+            return 1;
         return 2;
     }
 
-    void publishStateIfChanged(unsigned int newState) {
-        if (newState != currentState) {
+    void publishStateIfChanged(unsigned int newState)
+    {
+        if (newState != currentState)
+        {
             currentState = newState;
             outputDoc.clear();
-            outputDoc["state"]["reported"]["devices"][THING_NAME]["heart_rate_state"] = currentState;
+            outputDoc["state"]["reported"]["heart_rate_state"] = currentState;
             serializeJson(outputDoc, outputBuffer);
             client.publish(UPDATE_TOPIC, outputBuffer);
             Serial.print("Publicado nuevo estado: ");
@@ -101,23 +125,30 @@ public:
         }
     }
 
-    void setCertificates(const char *rootCA, const char *cert, const char *key) {
+    void setCertificates(const char *rootCA, const char *cert, const char *key)
+    {
         wiFiClient.setCACert(rootCA);
         wiFiClient.setCertificate(cert);
         wiFiClient.setPrivateKey(key);
     }
 
-    bool isConnected() {
+    bool isConnected()
+    {
         return client.connected();
     }
 
-    void connectMQTT() {
-        while (!client.connected()) {
+    void connectMQTT()
+    {
+        while (!client.connected())
+        {
             Serial.print("Intentando conexión MQTT...");
-            if (client.connect("CLIENT_ID")) {
+            if (client.connect("CLIENT_ID"))
+            {
                 Serial.println("conectado");
                 client.subscribe(UPDATE_DELTA_TOPIC);
-            } else {
+            }
+            else
+            {
                 Serial.print("falló, rc=");
                 Serial.print(client.state());
                 Serial.println(" intentando de nuevo en 5 segundos");
@@ -126,34 +157,39 @@ public:
         }
     }
 
-    void loop() {
+    void loop()
+    {
         client.loop();
     }
 
-    void updatePulseInShadow(int pulse) {
+    void updatePulseInShadow(int pulse)
+    {
         outputDoc.clear();
-        outputDoc["state"]["reported"]["devices"][THING_NAME]["heart_rate"] = pulse;
+        outputDoc["state"]["reported"]["heart_rate"] = pulse;
         serializeJson(outputDoc, outputBuffer);
         client.publish(UPDATE_TOPIC, outputBuffer);
     }
 
-    void publishPulseRequestAttended() {
+    void publishPulseRequestAttended()
+    {
         outputDoc.clear();
-        outputDoc["state"]["desired"]["devices"][THING_NAME]["pulse_requested"] = 0;
+        outputDoc["state"]["desired"]["pulse_requested"] = 0;
         serializeJson(outputDoc, outputBuffer);
         client.publish(UPDATE_TOPIC, outputBuffer);
     }
 
-    void reportMinPulseParameter() {
+    void reportMinPulseParameter()
+    {
         outputDoc.clear();
-        outputDoc["state"]["reported"]["devices"][THING_NAME]["min_pulse_alert"] = minPulseAlert;
+        outputDoc["state"]["reported"]["min_pulse_alert"] = minPulseAlert;
         serializeJson(outputDoc, outputBuffer);
         client.publish(UPDATE_TOPIC, outputBuffer);
     }
 
-    void reportMaxPulseParameter() {
+    void reportMaxPulseParameter()
+    {
         outputDoc.clear();
-        outputDoc["state"]["reported"]["devices"][THING_NAME]["max_pulse_alert"] = maxPulseAlert;
+        outputDoc["state"]["reported"]["max_pulse_alert"] = maxPulseAlert;
         serializeJson(outputDoc, outputBuffer);
         client.publish(UPDATE_TOPIC, outputBuffer);
     }
@@ -163,14 +199,16 @@ LCDDisplay lcd;
 MQTTHandler mqttHandler(MQTT_BROKER, MQTT_PORT);
 WiFiConnection wifi(WIFI_SSID, WIFI_PASS);
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     lcd.init();
-    
+
     pulseSensor.analogInput(35);
     pulseSensor.blinkOnPulse(2);
     pulseSensor.setThreshold(2300);
-    if (pulseSensor.begin()) {
+    if (pulseSensor.begin())
+    {
         Serial.println("PulseSensor initialized.");
     }
 
@@ -180,19 +218,21 @@ void setup() {
     mqttHandler.connectMQTT();
 }
 
-void loop() {
-    if (!mqttHandler.isConnected()) {
+void loop()
+{
+    if (!mqttHandler.isConnected())
+    {
         mqttHandler.connectMQTT();
     }
     mqttHandler.loop();
 
-    Utilities::nonBlockingDelay(200, []() {
+    Utilities::nonBlockingDelay(200, []()
+                                {
         if (pulseSensor.sawStartOfBeat()) {
             unsigned int pulse = pulseSensor.getBeatsPerMinute();
             Serial.print(pulse);
             unsigned int newState = mqttHandler.determinePulseState(pulse);
             mqttHandler.publishStateIfChanged(newState);
             lcd.printMessage(pulse, 0, 0);
-        }
-    });
+        } });
 }
